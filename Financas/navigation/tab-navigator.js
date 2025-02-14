a1529d843315133e6fe72e20d15379a443a82d47
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-14 15:08:19 -0300

    Personalizando paginas

--- Financas/navigation/tab-navigator.js
+++ Financas/navigation/tab-navigator.js
@@ -1,14 +1,31 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TelaInicial from '../pages/Inicio.js';
 import Lista from '../pages/Lista.js';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={TelaInicial} />
-      <Tab.Screen name="Lista" component={Lista} />
+      <Tab.Screen
+        name="Início"
+        component={TelaInicial}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Lista de Manutenções"
+        component={Lista}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- Financas/pages/Inicio.js
+++ Financas/pages/Inicio.js
@@ -1,6 +1,7 @@
 import { View, Text, TextInput, Button} from 'react-native';
 import React, { useState } from 'react';
 import {ImageBackground } from 'react-native';
+import { styles } from '../styles/style';
 
 const TelaInicial = () => {
   const [nome, setNome] = useState('');
@@ -20,32 +21,35 @@ const TelaInicial = () => {
     source={require('../assets/cifroes.jpeg')}
     style={{ width: '100%', height: '100%' }} 
     >
-        <View>
-        <Text>Digite o nome da manutenção:</Text>
-        <TextInput
-          value={nome}
-          onChangeText={(text) => setNome(text)}
-          placeholder="Ex: Troca de óleo"
-        />
-        <Text>Digite o valor:</Text>
-        <TextInput
-          value={valor}
-          onChangeText={(text) => setValor(text)}
-          placeholder="R$ 20,00"
-          keyboardType="numeric"
-        />
-        <Text>Digite a data do pagamento:</Text>
-        <TextInput
-          value={dataPagamento}
-          onChangeText={(text) => setDataPagamento(text)}
-          placeholder="(dd/mm/aaaa)"
-          keyboardType="numeric"
-        />
-        <Button title="Guardar" onPress={handleGuardarNome} />
-        <Text>Manutenção: {nomeGuardado}</Text>
-        <Text>Valor: {valorGuardado}</Text>
-        <Text>Data do pagamento: {dataPagamento}</Text>
-      </View>
+        <View style = {styles.inicio}>
+            <Text style = {styles.textoInicio}>Digite o nome da manutenção:</Text>
+            <TextInput
+            value={nome}
+            onChangeText={(text) => setNome(text)}
+            placeholder="Ex: Troca de óleo"
+            style = {styles.inputInicio}
+            />
+            <Text style = {styles.textoInicio}>Digite o valor:</Text>
+            <TextInput
+            style = {styles.inputInicio}
+            value={valor}
+            onChangeText={(text) => setValor(text)}
+            placeholder="R$ 20,00"
+            keyboardType="numeric"
+            />
+            <Text style = {styles.textoInicio}>Digite a data do pagamento:</Text>
+            <TextInput
+            style = {styles.inputInicio}
+            value={dataPagamento}
+            onChangeText={(text) => setDataPagamento(text)}
+            placeholder="(dd/mm/aaaa)"
+            keyboardType="numeric"
+            />
+            <Button color= 'green' title="Guardar Manutenção" onPress={handleGuardarNome} />
+            <Text>Manutenção: {nomeGuardado}</Text>
+            <Text>Valor: {valorGuardado}</Text>
+            <Text>Data do pagamento: {dataPagamento}</Text>
+        </View>
       </ImageBackground>
     )
 }
