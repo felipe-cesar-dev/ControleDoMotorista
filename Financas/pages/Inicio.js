7059008f202b5e02aed4c5001f365c9fb443e41c
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-14 16:46:48 -0300

    Update Inicio.js

--- Financas/pages/Inicio.js
+++ Financas/pages/Inicio.js
@@ -1,8 +1,7 @@
+import { View, Text, TextInput, Button} from 'react-native';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import * as SQLite from 'expo-sqlite';
-
-const db = SQLite.openDatabaseAsync('manutencao.db');
+import {ImageBackground } from 'react-native';
+import { styles } from '../styles/style';
 
 const TelaInicial = () => {
   const [nome, setNome] = useState('');
@@ -10,54 +9,33 @@ const TelaInicial = () => {
   const [dataPagamento, setDataPagamento] = useState('');
   const [manutencao, setManutencao] = useState({});
 
-  const criarTabela = async () => {
-    db.transaction(tx => {
-      tx.executeSql(`
-        CREATE TABLE IF NOT EXISTS manutencao (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          nome TEXT NOT NULL,
-          valor TEXT NOT NULL,
-          dataPagamento TEXT NOT NULL
-        );
-      `);
-    });
-  };
-
-  const guardarManutencao = async () => {
-    criarTabela();
-
+  const guardarManutencao = () => {
     const dados = {
       nome: nome,
       valor: valor,
       dataPagamento: dataPagamento,
     };
-
-    db.transaction(tx => {
-      tx.executeSql(`
-        INSERT INTO manutencao (nome, valor, dataPagamento)
-        VALUES (?, ?, ?);
-      `, [dados.nome, dados.valor, dados.dataPagamento]);
-    });
-
     setManutencao(dados);
   };
 
   return (
-    <View>
-      <Text>Digite o nome da manutenção:</Text>
-      <TextInput value={nome} onChangeText={(text) => setNome(text)} placeholder="Ex: Troca de óleo" />
-      <Text>Digite o valor:</Text>
-      <TextInput value={valor} onChangeText={(text) => setValor(text)} placeholder="R$ 20,00" keyboardType="numeric" />
-      <Text>Digite a data do pagamento:</Text>
-      <TextInput value={dataPagamento} onChangeText={(text) => setDataPagamento(text)} placeholder="(dd/mm/aaaa)" keyboardType="numeric" />
-      <Button title="Guardar Manutenção" onPress={guardarManutencao} />
-      <View>
-        <Text>Manutenção:</Text>
-        <Text>Nome: {manutencao.nome}</Text>
-        <Text>Valor: {manutencao.valor}</Text>
-        <Text>Data do pagamento: {manutencao.dataPagamento}</Text>
+    <ImageBackground source={require('../assets/cifroes.jpeg')} style={{ width: '100%', height: '100%' }} >
+      <View style = {styles.inicio}>
+        <Text style = {styles.textoInicio}>Digite o nome da manutenção:</Text>
+        <TextInput value={nome} onChangeText={(text) => setNome(text)} placeholder="Ex: Troca de óleo" style = {styles.inputInicio} />
+        <Text style = {styles.textoInicio}>Digite o valor:</Text>
+        <TextInput style = {styles.inputInicio} value={valor} onChangeText={(text) => setValor(text)} placeholder="R$ 20,00" keyboardType="numeric" />
+        <Text style = {styles.textoInicio}>Digite a data do pagamento:</Text>
+        <TextInput style = {styles.inputInicio} value={dataPagamento} onChangeText={(text) => setDataPagamento(text)} placeholder="(dd/mm/aaaa)" keyboardType="numeric" />
+        <Button color= 'green' title="Guardar Manutenção" onPress={guardarManutencao} />
+        <View>
+          <Text>Manutenção:</Text>
+          <Text>Nome: {manutencao.nome}</Text>
+          <Text>Valor: {manutencao.valor}</Text>
+          <Text>Data do pagamento: {manutencao.dataPagamento}</Text>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
