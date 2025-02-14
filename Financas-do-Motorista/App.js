805449261546feaf7c0e8c3a483843be6ce38513
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-14 12:05:43 -0300

    primeiros passos

--- Financas-do-Motorista/App.js
+++ Financas-do-Motorista/App.js
@@ -1,7 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, ImageBackground, SafeAreaView } from 'react-native';
+
 
 const App = () => {
+  
   const [nome, setNome] = useState('');
   const [nomeGuardado, setNomeGuardado] = useState('');
   const [valor, setValor] = useState('');
@@ -15,32 +18,44 @@ const App = () => {
   };
 
   return (
-    <View>
-      <Text>Digite seu nome:</Text>
-      <TextInput
-        value={nome}
-        onChangeText={(text) => setNome(text)}
-        placeholder="Nome"
-      />
-      <Text>Digite o valor:</Text>
-      <TextInput
-        value={valor}
-        onChangeText={(text) => setValor(text)}
-        placeholder="Valor"
-        keyboardType="numeric"
-      />
-      <Text>Digite a data do pagamento:</Text>
-      <TextInput
-        value={dataPagamento}
-        onChangeText={(text) => setDataPagamento(text)}
-        placeholder="Data do pagamento (dd/mm/aaaa)"
-        keyboardType="numeric"
-      />
-      <Button title="Guardar" onPress={RecolheDados} />
-      <Text>Nome guardado: {nomeGuardado}</Text>
-      <Text>Valor guardado: {valorGuardado}</Text>
-      <Text>Data do pagamento guardada: {dataPagamento}</Text>
-    </View>
+    
+    <ImageBackground
+      source={require('./assets/cifroes.jpeg')}
+      style={{ width: '100%', height: '100%' }}
+    > 
+      <SafeAreaView>
+        <View>
+          <Text>Digite seu nome:</Text>
+          <TextInput
+            value={nome}
+            onChangeText={(text) => setNome(text)}
+            placeholder="Nome"
+          />
+          <Text>Digite o valor:</Text>
+          <TextInput
+            value={valor}
+            onChangeText={(text) => setValor(text)}
+            placeholder="Valor"
+            keyboardType="numeric"
+          />
+          <Text>Digite a data do pagamento:</Text>
+          <TextInput
+            value={dataPagamento}
+            onChangeText={(text) => setDataPagamento(text)}
+            placeholder="Data do pagamento (dd/mm/aaaa)"
+            keyboardType="numeric"
+          />
+          <View style={{
+            padding: '20',
+          }}>
+            <Button color='green' title="Guardar dados" onPress={RecolheDados} />
+          </View>
+          <Text>Nome guardado: {nomeGuardado}</Text>
+          <Text>Valor guardado: {valorGuardado}</Text>
+          <Text>Data do pagamento guardada: {dataPagamento}</Text>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
